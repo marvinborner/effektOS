f927c48a210b18130b26ce541e7f6d7f8e6a26cd
Marvin Borner <git@marvinborner.de>
2026-09-10 22:51:26 +0200

    Towards stateless userspace

--- libraries/llvm/main.c
+++ libraries/llvm/main.c
@@ -22,12 +22,6 @@ static struct flanterm_context *ft_ctx = 0;
 
 #include "../limine.h"
 
-REQUEST static volatile struct limine_framebuffer_request framebuffer_request = {
-	.id = LIMINE_FRAMEBUFFER_REQUEST_ID,
-	.revision = 0,
-	.response = 0
-};
-
 REQUEST static volatile struct limine_memmap_request memmap_request = {
 	.id = LIMINE_MEMMAP_REQUEST_ID,
 	.revision = 0,
